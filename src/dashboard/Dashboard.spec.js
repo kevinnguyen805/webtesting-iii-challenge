6b9a9e04641e0647b73ea02a3adde7db40be1ff9
admin <admin@admins-MacBook-Pro.local>
2019-10-14 11:54:39 -0700

    snapshots for each file successful

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,14 @@
 // Test away
+
+import React from 'react'
+import * as rtl from 'react-testing-library'
+import 'jest-dom/extend-expect'
+import Dashboard from './Dashboard'
+afterEach(rtl.cleanup)
+
+describe("Dashboard", () => {
+     it('renders correctly', () => {
+          const wrapper = rtl.render(<Dashboard />)
+          expect(wrapper.baseElement).toMatchSnapshot();
+     })
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,14 @@
 // Test away!
+import React from 'react'
+import * as rtl from 'react-testing-library'
+import 'jest-dom/extend-expect'
+import Display from './Display'
+afterEach(rtl.cleanup)
+
+
+describe("Display", () => {
+     it("renders correctly", () => {
+          const wrapper = rtl.render(<Display />)
+          expect(wrapper.baseElement).toMatchSnapshot();
+     })
+})
